31bedb60cb93291f66be3fa82b87cb1d24884f79
prasad <ashoksurisetty0508@gmail.com>
2023-05-08 18:32:32 +0530

    learn terraform

--- 02-data sources/main.tf
+++ 02-data sources/main.tf
@@ -2,5 +2,5 @@ data "aws_security_group" "selected" {
   name = "allow-all1"
 }
 output "security_group_id" {
-  value = data.aws_security_group.selected.id
+  value = try(data.aws_security_group.selected.id,null)
 }
