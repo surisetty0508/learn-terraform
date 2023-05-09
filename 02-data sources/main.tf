dabeaa25aa65696f89b61e83100359da2f4c446c
prasad <ashoksurisetty0508@gmail.com>
2023-05-09 09:51:15 +0530

    learn terraform

--- 02-data sources/main.tf
+++ 02-data sources/main.tf
@@ -1,6 +1,11 @@
-data "aws_security_group" "selected" {
-  name = "allow-all1"
-}
-output "security_group_id" {
-  value = try(data.aws_security_group.selected.id,null)
-}
+//data "aws_security_group" "selected" {
+//  name = "allow-all1"
+//}
+//output "security_group_id" {
+//  value = data.aws_security_group.selected.id
+//}
+
+data "aws_security_groups" "test" {}
+  output "all_sg" {
+      value = data.aws_security_groups.test
+  }
