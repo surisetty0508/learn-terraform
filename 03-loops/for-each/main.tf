bb7c216b2fa832b4d79d601327866032a02a12a7
prasad <ashoksurisetty0508@gmail.com>
2023-05-11 16:09:45 +0530

    learn terraform

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -6,13 +6,36 @@ resource "null_resource" "fruits" {
   }
 }
 
+resource "null_resource" "fruits1" {
+  for_each = var.fruits1
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.key} - ${each.value["count"]}"
+
+  }
+}
+variable "fruits1" {
+  default = {
+    apple = {
+    name  = "apple"
+    count = "10"
+  }
+    orange = {
+    name = "orange"
+    count = 200
+  }
+    banana = {
+    name   = "banana"
+    count  = "100"
+    }
+  }
+}
 
 
 variable "fruits" {
   default = {
-    apple = 10
-    orange =200
+    apple  = 10
+    orange = 200
     banana = 300
   }
-
 }
