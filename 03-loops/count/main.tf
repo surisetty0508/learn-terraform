21b18224be6f1853169f84ec1940cefe128dc106
prasad <ashoksurisetty0508@gmail.com>
2023-05-11 11:08:17 +0530

    learn terraform

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -6,6 +6,11 @@ resource "null_resource" "fruits" {
   count = length(var.fruits)
 }
 
+ provisioner"local-exec"{
+  command = "echo Fruit Name - ${var.fruits[count.index]}"
+}
+
 variable "fruits" {
   default = ["apple","banana","orange"]
 }
+
